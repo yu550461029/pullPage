9c19387f772653463ff0f0f3ccdd3307748bc24b
yudagong <yu550461029@126.com>
2014-10-27 21:52:42 +0800

    first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PullPage::Application.config.secret_token = 'e32daca4bf968bd573c62e23d5a289acd2d0098ddbd930ba5f2b409d5e3ecab77b3ccc415083ba8c03754a40549038c3c9af07f89847243b111c30a70fd83341'
